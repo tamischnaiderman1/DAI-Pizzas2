b920cd5992f1699f3fa2829c9047ff01d6450fcb
TAMI VIOLETA SCHNAIDERMAN <47026792@ort>
2023-03-31 12:06:06 -0300

    primer clase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import { get2Pizzas } from './services/pizzaService.js';
+import { getAll, getById, insertById, deleteById, updateById } from './services/pizzaService.js';
 
-await get2Pizzas();
+await getAll();
+await getById();
+await insertById();
+await deleteById();
+await updateById();
--- src/services/pizzaService.js
+++ src/services/pizzaService.js
@@ -1,9 +1,41 @@
 import sql from 'mssql';
 import configDB from '../models/db.js';
 
-export const get2Pizzas = async () => {
+export const getAll = async () => {
     const conn = await sql.connect(configDB);
-    const results = await conn.request().query('SELECT TOP 2 * FROM Pizzas');
+    const results = await conn.request().query('SELECT * FROM Pizzas');
+
+    console.log(results);
+}
+
+
+export const getById = async () => {
+    const conn = await sql.connect(configDB);
+    const results = await conn.request().query('SELECT * FROM Pizzas WHERE ');
+
+    console.log(results);
+}
+
+
+export const updateById = async () => {
+    const conn = await sql.connect(configDB);
+    const results = await conn.request().query('UPDATE * FROM Pizzas WHERE ');
+
+    console.log(results);
+}
+
+
+export const deleteById = async () => {
+    const conn = await sql.connect(configDB);
+    const results = await conn.request().query('DELETE * FROM Pizzas WHERE ');
+
+    console.log(results);
+}
+
+
+export const insertById = async () => {
+    const conn = await sql.connect(configDB);
+    const results = await conn.request().query('INSERT * FROM Pizzas WHERE ');
 
     console.log(results);
 }
