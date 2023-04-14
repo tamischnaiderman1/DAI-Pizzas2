5be58337921e1f0bff5145c5d89fbc38b6b24564
LUCIANA WAREMKRAUT <47026652@ort>
2023-04-14 11:42:07 -0300

    express

--- src/index.js
+++ src/index.js
@@ -1,7 +1,50 @@
 import { getAll, getById, insert, deleteById, updateById } from './services/pizzaService.js';
+import express from "express"
+
+
+const app = express()
+const port = 3000
+
+app.get('/', async(req, res) => {
+    const PizzasRock = await getAll()
+    res.send(PizzasRock)
+})
+
+
+app.get('/:id', async(req, res) => {
+    const id = req.params.id
+    const PizzasRock = await getById(id)
+    res.send(PizzasRock)
+})
+
+
+app.delete('/:id', async(req, res) => {
+    const id = req.params.id
+    const PizzasPop = deleteById(id)
+    res.send(PizzasPop)
+})
+
+
+app.post('/', async(req, res) => {
+    const PizzasDisco = insert("tami", "sin tacc", true, 25)
+    res.send(PizzasDisco)
+})
+
+app.put('/:id', async(req, res) => {
+    const id = req.params.id
+    const PizzasReggaeton = updateById( "tami", "sin tacc", true, 25)
+    res.send(PizzasReggaeton)
+})
+
+
+app.listen(port, async( ) => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
 
 //await getAll();
 //await getById(4);
 //await insert("tami", ""sin tacc", true, 25);
 //await deleteById(11);
-await updateById(10, "tami", "sin tacc", true, 25);
+//await updateById(10, "tami", "sin tacc", true, 25);
--- src/services/pizzaService.js
+++ src/services/pizzaService.js
@@ -5,7 +5,7 @@ export const getAll = async () => {
     const conn = await sql.connect(configDB);
     const results = await conn.request().query('SELECT * FROM Pizzas');
 
-    console.log(results);
+    return results;
 }
 
 
@@ -13,7 +13,7 @@ export const getById = async (id) => {
     const conn = await sql.connect(configDB);
     const results = await conn.request().input("pId", id).query('SELECT * FROM Pizzas WHERE @pId = id');
 
-    console.log(results);
+    return results;
 }
 
 
@@ -26,7 +26,7 @@ export const updateById = async (id, nombre, descripcion, libreGluten, importe) 
     .input("pImporte", importe)
     .query('UPDATE Pizzas SET Nombre = @pNombre, Descripcion = @pDescripcion, LibreGluten = @pLibreGluten, Importe = @pImporte  WHERE @pId = id ');
 
-    console.log(results);
+    return results;
 }
 
 
@@ -34,7 +34,7 @@ export const deleteById = async (id) => {
     const conn = await sql.connect(configDB);
     const results = await conn.request().input("pId", id).query('DELETE FROM Pizzas WHERE @pId = id');
 
-    console.log(results);
+    return results;
 }
 
 
@@ -47,5 +47,5 @@ export const insert = async (nombre, descripcion, libreGluten, importe) => {
     .input("pImporte", importe)
     .query('INSERT INTO Pizzas (Nombre, Descripcion, LibreGluten, Importe) VALUES (@pNombre, @pDescripcion, @pLibreGluten, @pImporte)');
 
-    console.log(results);
+    return results;
 }
